b251033d3e89661b5553ce3d34fa430845e31b2d
ALEX JONES DA SILVA <43755501805@cmdo.policiamilitar.sp.gov.br>
2020-05-13 17:42:28 -0300

    inicio do back end

--- ALMSolutions/financeiro/models.py
+++ ALMSolutions/financeiro/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Entrada(models.Model):
+    valor_evento = models.DecimalField(decimal_places=2)
+    data_evento = models.DateTimeField('Data do vento')
+
+class Saida(models.Model):
+    pagamento_parceiro = models.DecimalField(decimal_places=2)
+    data_pagamento = models.DateTimeField('Data do pagamento')
