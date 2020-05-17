f6df7c59a06637bfd69609050a1e5f3f19e1b53c
Alex Jones <alex.silva@aluno.faculdadeimpacta.com.br>
2020-05-17 13:22:46 -0300

    CRUD Eventos Completo

--- ALMSolutions/agenda/urls.py
+++ ALMSolutions/agenda/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . import views
+from .views import consulta_evento, criar_evento, atualizar_evento, apagar_evento
 
-app_name ='financeiro'
 urlpatterns = [
-    path('', views.consulta_evento, name='consulta_evento'),
-    path('criar_evento', views.criar_evento, name='criar_evento'),
+    path('', consulta_evento, name='consulta_evento'),
+    path('criar_evento', criar_evento, name='criar_evento'),
+    path('atualizar_evento/<int:id>/', atualizar_evento, name='atualizar_evento'),
+    path('apagar_evento/<int:id>/', apagar_evento, name='apagar_evento'),
 ]
--- ALMSolutions/agenda/views.py
+++ ALMSolutions/agenda/views.py
@@ -16,3 +16,21 @@ def criar_evento(request):
         form.save()
         return redirect ('/agenda' )
     return render(request, 'agenda/formulario-evento.html', {'form':form})
+
+
+def atualizar_evento(request, id):
+    evento = Evento.objects.get(id = id)
+    form = FormularioEvento(request.POST or None, instance=evento)
+    #Valida se o formulario está em branco
+    if form.is_valid():
+        form.save()
+        return redirect ('/agenda')
+    return render(request, 'agenda/formulario-evento.html', {'form': form, 'evento': evento })
+
+
+def apagar_evento(request, id):
+    evento = Evento.objects.get(id = id)
+    if request.method == 'POST':
+        evento.delete()
+        return redirect ('/agenda')
+    return render(request, 'agenda/apagar_evento.html', { 'evento': evento })
